484156d99da862bdc694cfa89ea6ecfb59b21adf
ManGysITI <mangysiti@gmail.com>
2017-09-29 18:59:36 +0300

    Implement function check(str, bracketsConfig)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    'use strict';
+
+    if (str.length % 2 != 0)
+        return false;
+
+    let string = str.split('');
+    let bufferStack = [];
+
+    for(let i = 0; i < str.length; i++){
+        for(let j = 0; j < bracketsConfig.length; j++){
+            //console.log("str[i]="+str[i]+" === bracket[j][1]="+bracketsConfig[j][1]+" && bufferStack="+bufferStack[bufferStack .length - 1]+ "=== bracket[j][0]="+bracketsConfig[j][0]);
+
+            if (string[i] === bracketsConfig[j][1] && bufferStack[bufferStack.length - 1] === bracketsConfig[j][0])
+                bufferStack.pop();
+            else if (string[i] === bracketsConfig[j][0])
+                bufferStack.push(string[i]);
+        }
+    }
+    return bufferStack.length === 0 ? true : false;
 }
